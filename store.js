bf6c2d349a0f185b004ca9ae3ef40150975c7437
Gidong Kwon <lucidfext@gmail.com>
2025-05-04 01:03:17 +0900

    Change store type to Map

--- store.js
+++ store.js
@@ -5,24 +5,30 @@
  * 이벤트 목록
  * - added: 추가된 Row를 담고 있습니다.
  * - removed: 삭제된 Row를 담고 있습니다.
+ * - changed: 일부가 변경되었을 때 발생합니다. 바뀐 Row의 배열을 담고 있습니다.
+ * - overwritten: 전체가 덮어씌워졌을 때 발생합니다. 전체 데이터를 담고 있습니다.
  */
 class Store extends EventTarget {
-  data = [];
+  /**
+   * @type {Map<number, number>}
+   */
+  _data = new Map();
 
   constructor() {
     super();
   }
 
-  addPair(id, value) {
+  addEntry(id, value) {
     if (typeof id !== "number") {
-      console.error("id는 숫자여야 합니다.");
-      return;
+      throw new TypeError("id는 숫자여야 합니다.");
     }
     if (typeof value !== "number") {
-      console.error("value는 숫자여야 합니다.");
-      return;
+      throw new TypeError("value는 숫자여야 합니다.");
     }
-    this.data.push({ id, value });
+    if (this._data.has(id)) {
+      throw new Error("중복된 id입니다.");
+    }
+    this._data.set(id, value);
 
     this.dispatchEvent(
       new CustomEvent("added", {
@@ -31,18 +37,65 @@ class Store extends EventTarget {
     );
   }
 
-  removePair(id) {
-    const indexToRemove = this.data.findIndex((pair) => pair.id === id);
-    const [removed] = this.data.splice(indexToRemove, 1);
+  removeEntry(id) {
+    const removedValue = this._data.get(id);
+    const isRemoved = this._data.delete(id);
 
+    if (isRemoved) {
+      this.dispatchEvent(
+        new CustomEvent("removed", {
+          detail: { id, value: removedValue },
+        })
+      );
+    }
+  }
+
+  changeEntries(pairs) {
+    this._validatePairs(pairs);
+
+    for (const pair of pairs) {
+      this._data.set(pair.id, pair.value);
+    }
+  }
+
+  getAsJSON() {
+    return JSON.stringify(
+      [...this._data.entries()].map((v) => ({ id: v[0], value: v[1] })),
+      null,
+      2
+    );
+  }
+
+  /**
+   * @param {string} json 데이터 전체를 덮어쓸 json 문자열
+   */
+  setJSON(json) {
+    const parsedPairs = JSON.parse(json);
+    this._validatePairs(parsedPairs);
+    this._data = new Map(parsedPairs.map((v) => [v.id, v.value]));
     this.dispatchEvent(
-      new CustomEvent("removed", {
-        detail: removed,
+      new CustomEvent("overwritten", {
+        detail: parsedPairs,
       })
     );
   }
 
-  getAsJSON() {
-    return JSON.stringify(this.data, null, 2);
+  _validatePairs(pairs) {
+    if (!Array.isArray(pairs)) {
+      throw new TypeError("제공된 값이 배열 형식이 아닙니다.");
+    }
+    const isEveryElementValid = pairs.every((v) => {
+      const isObject = typeof v === "object";
+      const hasValidId = Object.hasOwn(v, "id") && typeof v.id === "number";
+      const hasValidValue =
+        Object.hasOwn(v, "value") && typeof v.value === "number";
+
+      return isObject && hasValidId && hasValidValue;
+    });
+    if (!isEveryElementValid) {
+      throw new TypeError(
+        "배열 내부의 값은 { id: number, value: number } 형식이어야 합니다."
+      );
+    }
   }
 }
